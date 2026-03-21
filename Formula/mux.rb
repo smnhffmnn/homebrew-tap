74cfe813baaf11b90ce3ae654bbe3621c26b2d78
Simon Hoffmann <simon@wvmd.de>
2026-03-21 15:15:07 +0100

    Update mux to 0.1.0

--- Formula/mux.rb
+++ Formula/mux.rb
@@ -1,26 +1,14 @@
 class Mux < Formula
   desc "Single-binary MCP gateway for databases, APIs, and tunnels"
   homepage "https://github.com/smnhffmnn/mux"
+  version "0.1.0"
   license "MIT"
 
-  # This formula is automatically updated by GoReleaser on new releases.
-  # Manual installation: download the latest release from GitHub.
+  depends_on :macos
+  depends_on arch: :arm64
 
-  on_macos do
-    if Hardware::CPU.arm?
-      url "https://github.com/smnhffmnn/mux/releases/latest/download/mux_darwin_arm64.tar.gz"
-    else
-      url "https://github.com/smnhffmnn/mux/releases/latest/download/mux_darwin_amd64.tar.gz"
-    end
-  end
-
-  on_linux do
-    if Hardware::CPU.arm?
-      url "https://github.com/smnhffmnn/mux/releases/latest/download/mux_linux_arm64.tar.gz"
-    else
-      url "https://github.com/smnhffmnn/mux/releases/latest/download/mux_linux_amd64.tar.gz"
-    end
-  end
+  url "https://github.com/smnhffmnn/mux/releases/download/v0.1.0/mux_0.1.0_darwin_arm64.tar.gz"
+  sha256 "1c5d7d1302c227ece01e36eecbf33d3aafe90bdbf05fa29aad20a7efeccac841"
 
   def install
     bin.install "mux"
