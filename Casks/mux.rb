0a6ac23a3a924c22758e8ba41abca2f10222a8e6
Simon Hoffmann <simon@wvmd.de>
2026-03-21 18:59:09 +0100

    Update mux to 0.4.0: formula + cask with .app bundle

--- Casks/mux.rb
+++ Casks/mux.rb
@@ -1,6 +1,6 @@
 cask "mux" do
   version "0.4.0"
-  sha256 :no_check
+  sha256 "5c45dfa216235098815879903eb3b38ed8250a658c9d8219d05c44c21fb2f9e2"
 
   url "https://github.com/smnhffmnn/mux/releases/download/v#{version}/mux_#{version}_darwin_arm64_app.tar.gz"
   name "mux"
--- Formula/mux.rb
+++ Formula/mux.rb
@@ -1,14 +1,14 @@
 class Mux < Formula
   desc "Single-binary MCP gateway for databases, APIs, and tunnels"
   homepage "https://github.com/smnhffmnn/mux"
-  version "0.3.0"
+  version "0.4.0"
   license "MIT"
 
   depends_on :macos
   depends_on arch: :arm64
 
-  url "https://github.com/smnhffmnn/mux/releases/download/v0.3.0/mux_0.3.0_darwin_arm64.tar.gz"
-  sha256 "01e5c60e20f8370fec9dc7420427519c1f62485d17f5a26b73d8d549b05cc7e2"
+  url "https://github.com/smnhffmnn/mux/releases/download/v0.4.0/mux_0.4.0_darwin_arm64.tar.gz"
+  sha256 "1cb169610eeb5ae597a9dccb716161c66806f517a73cb8fedbcbf56415cb9484"
 
   def install
     bin.install "mux"
