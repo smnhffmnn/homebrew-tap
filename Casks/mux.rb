056ad7fb320d985562bf2f5fadbea9274bb96483
Simon Hoffmann <simon@wvmd.de>
2026-03-21 18:45:53 +0100

    Add mux cask for macOS .app installation

---
+++ Casks/mux.rb
@@ -0,0 +1,17 @@
+cask "mux" do
+  version "0.4.0"
+  sha256 :no_check
+
+  url "https://github.com/smnhffmnn/mux/releases/download/v#{version}/mux_#{version}_darwin_arm64_app.tar.gz"
+  name "mux"
+  desc "Single-binary MCP gateway for databases, APIs, and tunnels"
+  homepage "https://github.com/smnhffmnn/mux"
+
+  depends_on arch: :arm64
+
+  app "mux.app"
+
+  zap trash: [
+    "~/.mux",
+  ]
+end
