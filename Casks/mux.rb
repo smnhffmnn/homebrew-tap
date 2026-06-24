0c362f9ebe489766f047aaf0c29b09b53799f9e6
Simon Hoffmann <simon@wvmd.de>
2026-06-24 13:56:23 +0200

    Update mux to 0.32.0

--- Casks/mux.rb
+++ Casks/mux.rb
@@ -1,8 +1,8 @@
 cask "mux" do
-  version "0.31.1"
-  sha256 "43fe78fcd2228d037159271862f7f333930d964beab7dfd72698782aa69da4f3"
+  version "0.32.0"
+  sha256 "7c87dcbeb68feae6e2ea4139da88ff6e43f77242ec7e7e65815ba2eabe69a4d4"
 
-  url "https://github.com/smnhffmnn/mux/releases/download/v0.31.1/mux_0.31.1_darwin_arm64_app.tar.gz"
+  url "https://github.com/smnhffmnn/mux/releases/download/v0.32.0/mux_0.32.0_darwin_arm64_app.tar.gz"
   name "mux"
   desc "Single-binary MCP gateway for databases, APIs, and tunnels"
   homepage "https://github.com/smnhffmnn/mux"
--- Formula/mux.rb
+++ Formula/mux.rb
@@ -1,14 +1,14 @@
 class Mux < Formula
   desc "Single-binary MCP gateway for databases, APIs, and tunnels"
   homepage "https://github.com/smnhffmnn/mux"
-  version "0.31.1"
+  version "0.32.0"
   license "MIT"
 
   depends_on :macos
   depends_on arch: :arm64
 
-  url "https://github.com/smnhffmnn/mux/releases/download/v0.31.1/mux_0.31.1_darwin_arm64.tar.gz"
-  sha256 "25246d59fefc3a533dfa44b5b765319461ec966658dc45753f37fc5f806594ba"
+  url "https://github.com/smnhffmnn/mux/releases/download/v0.32.0/mux_0.32.0_darwin_arm64.tar.gz"
+  sha256 "06b35d0cccc332abf0457bafae40d7bdd76a624369dad9e8769e42ffb7c8209a"
 
   def install
     bin.install "mux"
